a7590e47996b41a0e38628623023c2c4cd7b6735
Yun Heo <kokona0306@gmail.com>
2025-05-05 18:22:28 +0900

    Fix route order and add guestbook display

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
 
+messages = [
+    "Yun: Hello there!",
+    "GPT: Welcome to the guestbook :)"
+]
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, Flask!'
 
+@app.route('/guestbook')
+def guestbook():
+    return '<br>'.join(messages)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
